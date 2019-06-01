15b2f47eb89d33ce3b1c238d43678d54240d6c01
ZloyLeva <zloyleva@gmail.com>
2019-06-01 11:52:34 +0300

    create motion for Particles

--- app.js
+++ app.js
@@ -3,30 +3,50 @@
 		const canvasEl = document.getElementById('canvas');
 		const canvas = canvasEl.getContext('2d');
 
-		const height =canvasEl.height = window.innerHeight;
+		const height = canvasEl.height = window.innerHeight;
 		const width = canvasEl.width = window.innerWidth;
 
-		function Particle(){
-			this.y = Math.random()*height;
-			this.x = Math.random()*width;
+		function Particle() {
+			/** @property @private y */
+			let y = Math.random() * height;
+			/** @property x */
+			let x = Math.random() * width;
 
-			this.radius = Math.random()*5;
+			let radius = Math.random() * 5;
 
-			this.draw = function(){
+			let speed = 1 + Math.random()*4;
+			let angle = Math.floor(Math.random()*360);
+
+			let d = {
+				dx: Math.cos(angle)*speed,
+				dy: Math.sin(angle)*speed,
+			}
+
+			let update = function(){
+				x += d.dx;
+				y += d.dy;
+			}
+
+			this.draw = function () {
 				canvas.beginPath();
-				canvas.arc(this.x, this.y, this.radius, 0, Math.PI*2);
+				canvas.arc(x, y, radius, 0, Math.PI * 2);
 				canvas.closePath();
 				canvas.fillStyle = '#333';
 				canvas.fill();
+				update();
 			}
 		}
 
-		const particlesArray = [...Array(200)].map(()=>{
-			let partilce = new Particle();
-			partilce.draw();
-			return partilce;
-		})
+		const particlesArray = [...Array(200)].map(() => new Particle())
 
-		debugger
+		function loop(){
+			canvas.fillStyle = '#aaa';
+			canvas.fillRect(0, 0, width, height);
+
+			particlesArray.map(el => el.draw());
+			window.requestAnimationFrame(loop);
+		}
+		
+		loop();
 	}
 )()
